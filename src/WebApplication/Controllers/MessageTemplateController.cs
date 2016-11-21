6e0484cfcd12a73428df968765141a298f744884
David Silwal <de.davidsilwal@hotmail.com>
2016-11-21 21:00:11 +0545

    feature prototype preview: message template and useractivity detection

---
+++ src/WebApplication/Controllers/MessageTemplateController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Org.BouncyCastle.Asn1.Crmf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Data;
+
+namespace WebApplication.Controllers
+{
+    public class MessageTemplateController : Controller
+    {
+        public MessageTemplateController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        protected readonly ApplicationDbContext _context;
+
+        public IActionResult Index()
+        {
+           var item = AllMessageTemplate();
+
+            return View(item);
+        }
+
+        public IList<MessageTemplate> AllMessageTemplate()
+        {
+            var msg = _context.Database.GetCollection<MessageTemplate>("MessageTemplates").Aggregate().ToList();
+            return msg;
+        }
+        
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(MessageTemplate model)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.MessageTemplateCollection.InsertOne(model);
+            }
+
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ src/WebApplication/Data/MessageTemplate.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Data
+{
+    public class MessageTemplate
+    {
+        public ObjectId _id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+
+    }
+}
