604aec3f82df2ed8c21fb20bc890ea55e161c41c
David Silwal <de.davidsilwal@hotmail.com>
2016-11-23 22:30:00 +0545

    feature added : email service is working

--- src/WebApplication/Services/AuthMessageSender.cs
+++ src/WebApplication/Services/AuthMessageSender.cs
@@ -14,7 +14,7 @@ namespace WebApplication.Services
         {
             var emailMessage = new MimeMessage();
             var     message = new MimeMessage();
-            message.From.Add(new MailboxAddress("David Silwal", "de.davidsilwal@hotmail.com"));
+            message.From.Add(new MailboxAddress("David Silwal", "msunuwar11@outlook.com"));
             message.To.Add(new MailboxAddress("David Silwal", email));
             message.Subject = subject;
             message.Body = new TextPart("plain")
@@ -26,7 +26,7 @@ namespace WebApplication.Services
             {
                 client.Connect("smtp.live.com", 587, false);
                 client.AuthenticationMechanisms.Remove("XOAUTH2");
-                client.Authenticate("example@hotmail.com", "password");
+                client.Authenticate("msunuwar11@outlook.com", "9843104350Qwe123");
                 client.Send(message);
                 client.Disconnect(true);
             }
